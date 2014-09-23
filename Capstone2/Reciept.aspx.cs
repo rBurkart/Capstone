5dcc0d7d2ece1f2c586be486d8a478821c04489a
Tyler Edelman <tylernhl@comcast.net>
2014-09-23 17:52:13 -0400

    Revert "Revert "receipt page added""
    This reverts commit 8c927fe3b5094ba0f660af58a1d0df969a214d65.

---
+++ Capstone2/Reciept.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Reciept : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnBackToHome_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Default.aspx");
+    }
+}
